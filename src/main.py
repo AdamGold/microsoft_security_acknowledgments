b5c75f1e438f677947a9815d5f4fd3f460d82161
Adam Goldschmidt <adamgold7@gmail.com>
2020-02-01 22:20:47 +0200

    add documentations

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ from src.consts import FIELDS, FIRST_YEAR, FILE_NAME
 
 
 def get_max_cvss(products: list, vuln_instance: Vulnerability):
-    # get max CVSS score
+    """get max CVSS score from products list"""
     try:
         base_scores = [
             float(product["baseScore"]) for product in products if product["baseScore"]
@@ -27,12 +27,13 @@ def get_max_cvss(products: list, vuln_instance: Vulnerability):
 async def get_detailed_vulnerability(
     session: aiohttp.ClientSession, vuln_instance: Vulnerability
 ):
+    """send a request to a more detailed URL and insert to vulnerability object"""
     vuln_instance.data_url = f"https://portal.msrc.microsoft.com/api/security-guidance/en-us/CVE/{vuln_instance.cve_id}"
     vuln_instance.display_url = f"https://portal.msrc.microsoft.com/en-us/security-guidance/advisory/{vuln_instance.cve_id}"
     # visit URL
     more_info_response = await session.get(vuln_instance.data_url)
     result = await more_info_response.text()
-    # parse HTML to get desc, last updated, max CVSS score, exploited
+    # parse JSON to get desc, last updated, max CVSS score, exploited
     detailed = json.loads(result)
     vuln_instance.desc = cleanhtml(detailed["description"].split("\n")[0])
     vuln_instance.exploited = detailed["exploited"]
@@ -40,7 +41,9 @@ async def get_detailed_vulnerability(
         get_max_cvss(products, vuln_instance)
 
 
-async def parse_year_xml(session: aiohttp.ClientSession, writer, year: int):
+async def parse_year_json(session: aiohttp.ClientSession, writer, year: int):
+    """parse the main JSON of each year document that includes the basic information
+    of all vulnerabilities"""
     url = f"https://portal.msrc.microsoft.com/api/security-guidance/en-us/acknowledgments/year/{year}"
     response = await session.get(url)
     result = await response.text()
@@ -57,16 +60,17 @@ async def parse_year_xml(session: aiohttp.ClientSession, writer, year: int):
 
         if cveNumber:
             await get_detailed_vulnerability(session, vuln_instance)
-            writer.writerow(vuln_instance.list_of_attrs())
+            writer.writerow(vuln_instance.list_of_attrs)
 
 
 async def scan():
+    """main function to gather asyncio tasks and run them concurrently"""
     with open(FILE_NAME, "w",) as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow([field.capitalize().replace("_", " ") for field in FIELDS])
         async with aiohttp.ClientSession() as session:
             tasks = [
-                parse_year_xml(
+                parse_year_json(
                     session=session, writer=writer, year=FIRST_YEAR + year_inc
                 )
                 for year_inc in range(6)
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import re
 
 
 def cleanhtml(raw_html):
+    """clean string of HTML tags"""
     cleanr = re.compile("<.*?>")
     cleantext = re.sub(cleanr, "", raw_html)
     return cleantext
